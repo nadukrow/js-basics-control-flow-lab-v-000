d0ffaf983adf9af252bce05744af0829d5d0c7f2
Nana Adu-Krow <nadukrow@gmail.com>
2018-04-18 22:34:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // Write your code in this file!
 
-function scuberGreetingforFeet(distancetravel) {
-  if (distancetravel <= 400) {
+function scuberGreetingForFeet(number){
+  if (number <= 400){
     return "This one is on me!"
+  } else if ((2500 > number) && (number > 2000)) {
+    return "I will gladly take your thirty bucks."
+  } else if (number > 2500) {
+    return "No can do."
   }
 }

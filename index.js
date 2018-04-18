75aae296a883ddf9e5dde884d350400cf1b4c2d6
Nana Adu-Krow <nadukrow@gmail.com>
2018-04-18 22:45:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ function scuberGreetingForFeet(distancetravel){
   }
 }
 
-function ternaryCheckCity(city) {
-  return city === `NYC` ? `Ok, sounds good.` : `No go`;
-  }
+function ternaryCheckCity(city){
+  return city === "NYC" ? "Ok, sounds good." : "No go."
+}
